9f4bc3c5493bd99e2c387f9ca453389d467ae8e8
trangology <trangology@gmail.com>
2020-06-16 20:06:54 +0300

    add new overloading operators

--- homework02/safe_long_long.cpp
+++ homework02/safe_long_long.cpp
@@ -13,22 +13,6 @@ class safe_long_long_t {
             return *this;
         }
  
-        safe_long_long_t operator+(const safe_long_long_t &n) {
-            return safe_long_long_t(this->num+n.num);
-        }
- 
-        safe_long_long_t operator-(const safe_long_long_t &n) {
-            return safe_long_long_t(this->num-n.num);
-        }
- 
-        safe_long_long_t operator*(const safe_long_long_t &n) {
-            return safe_long_long_t(this->num*n.num);
-        }
- 
-        safe_long_long_t operator/(const safe_long_long_t &n) {
-            return safe_long_long_t(this->num/n.num);
-        }
- 
         long long get_num() {
             return this->num;
         }
@@ -36,6 +20,19 @@ class safe_long_long_t {
         void update_num(long long n) {
             this->num = n;
         }
+
+        // safe_long_long_t &operator++();
+        // safe_long_long_t &operator--();
+
+        // safe_long_long_t &operator*(const safe_long_long_t &n);
+        
+        // safe_long_long_t &operator+(const safe_long_long_t &n);
+        // safe_long_long_t &operator-(const safe_long_long_t &n);
+
+        // safe_long_long_t &operator+=(const safe_long_long_t &n);
+        // safe_long_long_t &operator-=(const safe_long_long_t &n);
+        // safe_long_long_t &operator*=(const safe_long_long_t &n);
+        // safe_long_long_t &operator/=(const safe_long_long_t &n);
  
         friend std::istream &operator>>(std::istream &in, safe_long_long_t n);
         friend std::ostream &operator<<(std::ostream &out, safe_long_long_t n);
@@ -43,7 +40,56 @@ class safe_long_long_t {
     private:
 	    long long num;
 };
+
+safe_long_long_t operator+=(safe_long_long_t &n1, safe_long_long_t &n2) {
+    long long result = n1.get_num() + n2.get_num();
+    return safe_long_long_t(result);
+}
+
+safe_long_long_t operator-=(safe_long_long_t &n1, safe_long_long_t &n2) {
+    return safe_long_long_t(n1.get_num() - n2.get_num());
+}
+
+safe_long_long_t operator++(safe_long_long_t &n) {
+    return safe_long_long_t(n.get_num() + 1);
+}
+
+safe_long_long_t operator--(safe_long_long_t &n) {
+    return safe_long_long_t(n.get_num() - 1);
+}
+
+safe_long_long_t operator+(safe_long_long_t &n1, safe_long_long_t &n2) {
+    return safe_long_long_t(n1.get_num() + n2.get_num());
+}
+
+safe_long_long_t operator-(safe_long_long_t &n1, safe_long_long_t &n2) {
+    return safe_long_long_t(n1.get_num() - n2.get_num());
+}
+
+safe_long_long_t operator*(safe_long_long_t &n1, safe_long_long_t &n2) {
+    return safe_long_long_t(n1.get_num() * n2.get_num());
+}
+
+safe_long_long_t operator/(safe_long_long_t &n1, safe_long_long_t &n2) {
+    return safe_long_long_t(n1.get_num() / n2.get_num());
+}
  
+bool operator==(safe_long_long_t &n1, safe_long_long_t &n2) {
+    return n1.get_num() == n2.get_num();
+}
+ 
+bool operator!=(safe_long_long_t &n1, safe_long_long_t &n2) {
+    return !(n1.get_num() == n2.get_num());
+}
+ 
+bool operator>=(safe_long_long_t &n1, safe_long_long_t &n2) {
+    return n1.get_num() >= n2.get_num();
+}
+ 
+bool operator<=(safe_long_long_t &n1, safe_long_long_t &n2) {
+    return n1.get_num() <= n2.get_num();
+}
+
 std::istream &operator>>(std::istream &in, safe_long_long_t n) 
 {
     long long num;
@@ -58,22 +104,6 @@ std::ostream &operator<<(std::ostream &out, safe_long_long_t n)
     return out;
 }
  
-bool operator==( safe_long_long_t &n1,   safe_long_long_t &n2) {
-    return n1.get_num() == n2.get_num();
-}
- 
-bool operator!=( safe_long_long_t &n1,   safe_long_long_t &n2) {
-    return !(n1.get_num() == n2.get_num());
-}
- 
-bool operator>=( safe_long_long_t &n1,   safe_long_long_t &n2) {
-    return n1.get_num() >= n2.get_num();
-}
- 
-bool operator<=( safe_long_long_t &n1,   safe_long_long_t &n2) {
-    return n1.get_num() <= n2.get_num();
-}
- 
  
 int main() {
     safe_long_long_t n1(5);
@@ -83,7 +113,13 @@ int main() {
     std::cout << n1 << " - " << n2 << " = " << n1 - n2 << '\n';
     std::cout << n1 << " * " << n2 << " = " << n1 * n2 << '\n';
     std::cout << n1 << " / " << n2 << " = " << n1 / n2 << '\n';
-    std::cout << (n1 == n2) << '\n';
+    std::cout << n1 << "++ = " << operator++(n1) << '\n';
+    std::cout << n2 << "-- =" << operator--(n2) << '\n';
+
+    if (n1 != n2) 
+    {
+        std::cout << "Are n1 and n2 different? Yes, they are!" << '\n';
+    }
  
     return 0;
 }
